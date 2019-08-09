3a7e4b5eb16b4891b48333f2a084d9dda729480e
Amine Boukhriss <boukhrissamine@gmail.com>
2019-08-09 02:48:35 +0200

    done with models

---
+++ db/migrate/20190809003915_change_description_to_be_string_in_dose.rb
@@ -0,0 +1,4 @@
+class ChangeDescriptionToBeStringInDose < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
---
+++ db/migrate/20190809004517_change_description_to_be_string_in_doses.rb
@@ -0,0 +1,4 @@
+class ChangeDescriptionToBeStringInDoses < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
